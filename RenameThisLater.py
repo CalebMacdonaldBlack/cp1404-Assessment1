3c8df4f966107ee492e57b18602285d289fc0e55
gigabytedx <calebmacdonaldblack@gmail.com>
2016-03-21 15:01:18 +1000

    finished first copy of sudo code

--- RenameThisLater.py
+++ RenameThisLater.py
@@ -14,11 +14,7 @@ function main()
             for item in item_file
                 print item removing commas, add selection index, format neatly and adding astrix if item is out
         else if choice is h
-                                                            item_list = return of display_and_get_item_list(choice, items_file)
-                                                            if item_list contains values
             hire_item(items_file)
-                                                            else
-                                                                print "All items are currently on hire"
         else if choice is r
             item_list = return of display_and_get_item_list(choice, items_file)
             if item_list contains values
@@ -33,16 +29,7 @@ function main()
         display menu
         get menu choice from user
     save items_file
-    print amount of items saved to items_file
-
-                                                            function display_and_get_item_list(choice, items_file)
-                                                                define new items list
-                                                                set index to 1
-                                                                for each item in items_file
-                                                                    put item in items
-
-                                                                output_items(items, choice)
-                                                                return items
+    print amount of items saved to items_file and a fairwell message
 
 function add_new_item()
     get item name from user
@@ -73,6 +60,7 @@ function hire_item(items_file)
         if item has not already been hired
             print item removing commas, add selection index, format neatly
             set has_item_been_listed to true
+
     if has_item_been_listed is false
         print "All items are currently on hire"
     else
@@ -85,20 +73,37 @@ function hire_item(items_file)
                 index_choice_is_a_number = true
             error value error
                 print invalid input error
-            
 
+        if item in items_file at the index of index_choice is able to be hired
+            print the selected items name and cost
+            set the item selected to "out" in items_file
+        else
+            print "That item is not available for hire"
 
-    if
+function return_item(items_file)
 
-function return_item(items)
+    set has_item_been_listed to false
+    for item in items_file
+        if item is currently out on hire
+            print item removing commas, add selection index, format neatly
+            set has_item_been_listed to true
 
-                                                function output_items(items, choice)
-                                                    if choice is a
-                                                        print "All items on file (* indicates item is currently out):"
-                                                    for each item in items
-                                                        if choice is a
-                                                            print item removing commas, add selection index, format neatly and adding astrix if item is out
-                                                        else
-                                                        if item is able to be chosen
-                                                            print item removing commas, add selection index, format neatly
+    if has_item_been_listed is false
+        print "No items are currently on hire"
+    else
+        print "Enter the number of the item to return"
+        set index_choice_is_a_number to false
+        while index_choice_is_valid is false
+            try
+                get index_choice from user
+                convert index_choice to number
+                index_choice_is_a_number = true
+            error value error
+                print invalid input error
+
+        if item in items_file at the index of index_choice is able to be hired
+            print the selected items name and say it was returned
+            set the item selected to "in" in items_file
+        else
+            print "That item has already been returned"
 """
