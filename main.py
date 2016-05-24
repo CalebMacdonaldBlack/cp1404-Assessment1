8a5dcdc4b61881357c2e28609e3052bb3d6cb704
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-05-24 19:28:07 +1000

    refactored and added comments to functions

--- main.py
+++ main.py
@@ -15,11 +15,19 @@ ITEM_OUT_COLOR = [2, 1, 1, 1]  # Red
 ITEM_IN_COLOR = [1, 2, 1, 1]  # Green
 ITEM_SELECTED_COLOR = [2, 2, 2, 1]  # Light Grey
 
+# TODO is this name correct?
+TITLE = 'Equipment Hire Gui'
+KV_FILE_NAME = 'app.kv'
+
 
 class EquipmentHireGui(App):
     status_text = StringProperty()
 
     def __init__(self):
+        """
+        Constructor
+        Initializes variables
+        """
         super(EquipmentHireGui, self).__init__()
         self.item_list = None  # this wont go into build
         self.program_state = State.LIST_ITEMS
@@ -28,15 +36,23 @@ class EquipmentHireGui(App):
         self.status_text = ''
 
     def build(self):
-        self.title = 'Hello World!'
-        self.root = Builder.load_file('app.kv')
+        self.title = TITLE
+        self.root = Builder.load_file(KV_FILE_NAME)
+
+        # add all items to the display
         for item in self.item_list.items:
             self.add_item_to_display(item)
+
         # press list_items to engage button and set status text
         self.list_items_pressed(self.root.ids.list_items)
         return self.root
 
     def item_button_pressed(self, button):
+        """
+        Invoked when an item button is pressed. This will select the item that was pressed
+        :param button:
+        :return:
+        """
         item = self.item_list.find_item_by_id(button.id)
         # this can all be reduced
         if self.program_state == State.HIRE_ITEMS or self.program_state == State.RETURN_ITEMS:
@@ -46,7 +62,7 @@ class EquipmentHireGui(App):
                     button.background_color = ITEM_OUT_COLOR
                     del self.items_selected_id_and_button_dictionary[button.id]
                 else:
-                    #button.background_color = ITEM_SELECTED_COLOR
+                    # button.background_color = ITEM_SELECTED_COLOR
                     button.state = 'down'
                     self.items_selected_id_and_button_dictionary[button.id] = button
             elif item.location == 'in' and self.program_state == State.HIRE_ITEMS:
@@ -54,7 +70,7 @@ class EquipmentHireGui(App):
                     button.background_color = ITEM_IN_COLOR
                     del self.items_selected_id_and_button_dictionary[button.id]
                 else:
-                    #button.background_color = ITEM_SELECTED_COLOR
+                    # button.background_color = ITEM_SELECTED_COLOR
                     button.state = 'down'
                     self.items_selected_id_and_button_dictionary[button.id] = button
 
@@ -64,33 +80,49 @@ class EquipmentHireGui(App):
             self.status_text = '{} ({}), ${:.2f} is {}'.format(item.name, item.description, item.price, item.location)
 
     def list_items_pressed(self, button):
-        self.release_buttons()
+        """
+        Invoked when the 'list items' button is pressed on the gui. Updates the programs state to 'LIST_ITEMS'.
+        :param button: The button that was pressed
+        """
+        self.release_menu_buttons()
         self.status_text = LIST_ITEMS_STATUS_MESSAGE
         button.state = 'down'
         # empty selected items list
-        self.deselect_item_buttons()
+        self.release_item_buttons()
         self.items_selected_id_and_button_dictionary = {}
         self.program_state = State.LIST_ITEMS
 
     def hire_items_pressed(self, button):
-        self.release_buttons()
+        """
+        Invoked when the 'hire item' button is pressed on the gui. Updates the programs state to 'HIRE_ITEMS'.
+        :param button: The button that was pressed
+        """
+        self.release_menu_buttons()
         button.state = 'down'
         # empty selected items list
-        self.deselect_item_buttons()
+        self.release_item_buttons()
         self.items_selected_id_and_button_dictionary = {}
         self.program_state = State.HIRE_ITEMS
         self.display_items_selected_in_status()
 
-    def return_items_pressed(self, button):
-        self.release_buttons()
+    def return_items_button_pressed(self, button):
+        """
+        Invoked when the 'return item' button is pressed on the gui. Updates the programs state to 'RETURN_ITEMS'.
+        :param button: The button that was pressed
+        """
+        self.release_menu_buttons()
         self.status_text = RETURN_ITEMS_STATUS_MESSAGE
         button.state = 'down'
         # empty selected items list
-        self.deselect_item_buttons()
+        self.release_item_buttons()
         self.items_selected_id_and_button_dictionary = {}
         self.program_state = State.RETURN_ITEMS
 
-    def confirm_pressed(self):
+    def confirm_button_pressed(self):
+        """
+        This is used to flip the location of the items that are selected and update the location property of the items
+        selected. This will also update the background color of item.
+        """
         if self.program_state == State.HIRE_ITEMS or self.program_state == State.RETURN_ITEMS:
             for button in self.root.ids.boxLayout_item_buttons.children:
                 if button.state == 'down':
@@ -105,30 +137,37 @@ class EquipmentHireGui(App):
 
                     self.display_items_selected_in_status()
 
-    def add_new_pressed(self):
-        self.release_buttons()
+    def add_new_button_pressed(self):
+        """
+        Invoked when the 'add new' button is pressed on the gui. This will open a popup window for adding a new item
+        :return:
+        """
+        self.release_menu_buttons()
         self.status_text = ADD_NEW_ITEM_STATUS_MESSAGE
         # empty selected items list
-        self.deselect_item_buttons()
+        self.release_item_buttons()
         self.items_selected_id_and_button_dictionary = {}
         self.root.ids.popup.open()
 
-    def release_buttons(self):
+    def release_menu_buttons(self):
+        """
+        releases (deselects) all menu buttons. Used when changing states.
+        """
         for button in self.root.ids.menu_buttons.children:
             button.state = 'normal'
 
-    def deselect_item_buttons(self):
+    def release_item_buttons(self):
+        """
+        releases (deselects) all item buttons. Mostly used when changing states.
+        """
         for button in self.root.ids.boxLayout_item_buttons.children:
             button.state = 'normal'
 
-        # for id, button in self.items_selected_id_and_button_dictionary.items():
-        #     item = self.item_list.find_item_by_id(id)
-        #     if item.location == 'out':
-        #         button.background_color = ITEM_OUT_COLOR
-        #     else:
-        #         button.background_color = ITEM_IN_COLOR
-
     def display_items_selected_in_status(self):
+        """
+        Used to display information about the current item/s selected. displayed information will change depending on
+        the current state of the program and what is selected.
+        """
         price = 0
         selected_item_names = []
         for id, button in self.items_selected_id_and_button_dictionary.items():
@@ -146,6 +185,14 @@ class EquipmentHireGui(App):
                 self.status_text = 'Select available items to return'
 
     def save_pressed(self, name, description, price):
+        """
+        Invoked when the save button is pressed on the 'create item' popup. if all fields are valid, it will create
+        and add the new item to the item list. It will also call the function to add the item to the item list gui, and
+        dismiss the popup window
+        :param name:
+        :param description:
+        :param price:
+        """
         if self.are_text_fields_valid(name, description, price):
             item = self.item_list.create_and_add_item(name, description, float(price))
             self.add_item_to_display(item)
@@ -154,6 +201,16 @@ class EquipmentHireGui(App):
             self.list_items_pressed(self.root.ids.list_items)
 
     def are_text_fields_valid(self, name, description, price):
+        """
+        Returns whether or not the text fields are valid as a boolean. Updates the status text label with the current
+        error message
+        :param name: name value of the item
+        :param description: description value for the item
+        :param price: price of the item
+        :return : True if all fields are valid and false if any of the fields aren't valid
+        """
+        # TODO is it a bad idea for this function update the status text and return a boolean? should I separate this?
+        # TODO Should I separate this into multiple verification functions?
         if name == '' or description == '' or price == '':
             self.status_text = 'All fields must be completed'
             return False
@@ -168,24 +225,40 @@ class EquipmentHireGui(App):
             return False
 
     def exit_popup_and_clear_fields(self):
+        """
+        Clears the values of the item field widgets and dismisses (exits) the 'create item' popup window
+        """
         self.root.ids.popup.dismiss()
         self.root.ids.item_name_input.text = ''
         self.root.ids.item_description_input.text = ''
         self.root.ids.item_price_input.text = ''
 
     def add_item_to_display(self, item):
+        """
+        Creates buttons for each item and add it to the item list in the gui. Sets the background color of that button
+        to green if the item is able to be hired (in) and to red if the item is not able to be hired (out)
+        :param item: The item instance that contains all the properties of the item button
+        """
         btn = Button(text=item.name)
         btn.bind(on_release=self.item_button_pressed)
         btn.size_hint = (1, None)
         btn.size = (0, 40)
+
+        # ID assigned to allow items with the same name and avoid weird behaviour
         btn.id = 'item_{}'.format(item.index)
+
         if item.location == 'out':
             btn.background_color = ITEM_OUT_COLOR
         else:
             btn.background_color = ITEM_IN_COLOR
+
         self.root.ids.boxLayout_item_buttons.add_widget(btn)
 
     def on_stop(self):
+        """
+        Invokes the save function on the item_list to save all the items and their current properties to the csv file
+        """
         self.item_list.save_items()
+
 
 EquipmentHireGui().run()
